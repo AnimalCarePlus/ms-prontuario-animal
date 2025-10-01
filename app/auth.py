47a406536a4f1ab5714db0d21f0cc3aa5606a615
Juan F <132524117+JuanF5@users.noreply.github.com>
2025-10-01 13:51:27 -0300

    Concluindo

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,37 @@
-from flask_jwt_extended import verify_jwt_in_request
-from flask import jsonify
+import requests
+from flask import request, jsonify
 from functools import wraps
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+AUTH_VALIDATE_URL = os.getenv("AUTH_VALIDATE_URL")
 
 def jwt_protected(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
+        auth_header = request.headers.get("Authorization")
+
+        if not auth_header:
+            return jsonify({"message": "Token não fornecido"}), 401
+
+        
+        token = auth_header.replace("Bearer ", "").strip()
+
         try:
-            verify_jwt_in_request()
-        except Exception:
-            return jsonify({'message': 'Token inválido ou ausente'}), 401
-        return fn(*args, **kwargs)
-    return wrapper
+            response = requests.get(
+                AUTH_VALIDATE_URL,
+                headers={"Authorization": token},
+                timeout=5
+            )
+
+            if response.status_code == 204:
+                
+                return fn(*args, **kwargs)
+            else:
+                return jsonify({"message": "Token inválido ou expirado"}), 401
+
+        except requests.RequestException as e:
+            return jsonify({"message": "Erro ao validar token", "error": str(e)}), 500
+
+    return wrapper
